af29f3b5932bd17ae6cc7781e8bfe6d5c3cb83b4
Steffen Kühne <stekhn@gmail.com>
2020-03-13 22:22:56 +0100

    Add HTML table to JSON converter

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const config = require('./config.json');
 
 const fetch = require('node-fetch');
-const { JSDOM } = require('jsdom');
-const tableToJSON = require('html-table-to-json');
+const cheerio = require('cheerio');
 // const { BigQuery } = require('@google-cloud/bigquery');
 
 // const bigquery = new BigQuery({
 //   projectId: config.projectId
 // });
 
-const html = getBody(config.url);
-const table = getTable(html);
+const html = getBody(config.url).catch(console.error);
+const data = getTableData(html).catch(console.error);
+console.log(data.then(console.log));
 
 // console.log(`Creating table ${config.bigQuery.tableId} in dataset ${config.bigQuery.datasetId}`);
 
@@ -20,13 +20,44 @@ const table = getTable(html);
 async function getBody(url) {
   return fetch(url)
     .then(res => res.text())
-    .catch(err => console.error(err));
+    .catch(console.error);
 }
 
-async function getTable(html) {
-  const dom = new JSDOM(await html);
-  const table = dom.window.document.querySelectorAll('table')[0];
-  const json = tableToJSON.parse(table.outerHTML);
-  console.log(json.results[0]);
+async function getTableData(html) {
+  const $ = cheerio.load(await html);
+  const tableHtml = $('table').parent().html();
+  const json = tableToJSON(tableHtml);
+
   return json;
+}
+
+async function tableToJSON(html) {
+  const $ = cheerio.load(await html);
+  const table = $('table');
+  let headers = [];
+  let results = [];
+  
+  // Build headers
+  $(table).find('tr').each((i, row) => {
+    $(row).find('th').each((j, cell) => {
+      headers[j] = $(cell).text().trim();
+    });
+  });
+
+  $(table).find('tr').first().find('td').each((j, cell) => {
+    headers[j] = $(cell).text().trim();
+  });
+
+  // Find rows
+  $(table).find('tr').each((index, row) => {
+    results[index] = {};
+    $(row).find('td').each((i, cell) => {
+      results[index][headers[i] || (i + 1)] = $(cell).text().trim();
+    });
+  });
+
+  // Remove empty rows
+  results = results.filter(t => Object.keys(t).length);
+  
+  return results;
 }
